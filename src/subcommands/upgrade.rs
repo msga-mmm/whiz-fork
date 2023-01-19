ebdec79cb7ce5f564a9f920d37cb09cf1dbbe30e
msga-mmm <michael.sga.mmm@gmail.com>
2023-01-19 17:53:33 +0000

    fix

--- src/subcommands/upgrade.rs
+++ src/subcommands/upgrade.rs
@@ -2,8 +2,8 @@ use self_update::cargo_crate_version;
 
 pub fn upgrade() -> Result<(), Box<dyn ::std::error::Error>> {
     let status = self_update::backends::github::Update::configure()
-        .repo_owner("zifeo")
-        .repo_name("whiz")
+        .repo_owner("msga-mmm")
+        .repo_name("whiz-fork")
         .bin_name("whiz")
         .show_download_progress(true)
         .current_version(cargo_crate_version!())
